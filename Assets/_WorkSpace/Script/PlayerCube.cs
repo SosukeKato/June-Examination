38491d9058479e1ba9bcf06190a0c10c1cb96296
dokindokinpiano <dokindokinpiano@gmail.com>
2025-05-26 18:20:06 +0900

    [Update]PlayerCubeScript更新

--- Assets/_WorkSpace/Script/PlayerCube.cs
+++ Assets/_WorkSpace/Script/PlayerCube.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerCube : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("ChangeScene", 1.5f);
     }
 
     // Update is called once per frame
