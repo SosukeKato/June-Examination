397a2e7e7e4eafd879b560c7d963c028f7a18dfd
dokindokinpiano <dokindokinpiano@gmail.com>
2025-05-26 18:21:15 +0900

    [Update]PlayerCubeScript内の不要な処理Invokeを削除

--- Assets/_WorkSpace/Script/PlayerCube.cs
+++ Assets/_WorkSpace/Script/PlayerCube.cs
@@ -6,7 +6,7 @@ public class PlayerCube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("ChangeScene", 1.5f);
+        
     }
 
     // Update is called once per frame
