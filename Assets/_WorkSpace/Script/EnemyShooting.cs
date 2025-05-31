1b0b3ea5a6396ad88484ab8c2e84ecbada4fa435
dokindokinpiano <dokindokinpiano@gmail.com>
2025-05-31 18:22:43 +0900

    [Update]EnemyBullet.csをBulletを生成できるように更新

--- Assets/_WorkSpace/Script/EnemyShooting.cs
+++ Assets/_WorkSpace/Script/EnemyShooting.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class EnemyShooting : MonoBehaviour
 {
-    [SerializeField] private Transform _muzzle;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField]
+    private GameObject _EnemyBulletPrefab;
+    [SerializeField]
+    private float _AttackInterval = 2f;
+    [SerializeField]
+    private List<Transform> _Muzzle;
+    private float _timer;
     // Update is called once per frame
     void Update()
     {
-        
+        _timer = Time.deltaTime;
+
+        if (_timer >= _AttackInterval)
+        {
+            int r = Random.Range(0, _Muzzle.Count);
+            Instantiate(_EnemyBulletPrefab, _Muzzle[r].position, Quaternion.identity);
+            _timer = 0;
+        }
     }
 }
