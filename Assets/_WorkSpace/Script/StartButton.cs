ebb8273b21cfc8832cd9189d54974e2e7060f394
dokindokinpiano <dokindokinpiano@gmail.com>
2025-05-31 14:51:26 +0900

    no message

--- Assets/_WorkSpace/Script/StartButton.cs
+++ Assets/_WorkSpace/Script/StartButton.cs
@@ -2,22 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class StartButton : MonoBehaviour
+public class SceneChanger : MonoBehaviour
 {
 
-    public void LoadScene(string PlayScene)
-    {
-        SceneManager.LoadScene(PlayScene);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void ChanegeScene(string StageName)
+    {   
+        SceneManager.LoadScene(StageName);
     }
 }
