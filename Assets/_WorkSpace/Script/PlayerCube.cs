10787aa777da98f6d79788d35bbc7be4d85e0dc9
dokindokinpiano <dokindokinpiano@gmail.com>
2025-06-02 17:30:32 +0900

    [fix]PlayerCube.cs不要な処理を削除 [fix]SceneChanger.cs不要な処理を削除

--- Assets/_WorkSpace/Script/PlayerCube.cs
+++ Assets/_WorkSpace/Script/PlayerCube.cs
@@ -5,18 +5,6 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerCube : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.CompareTag("EnemyBullet"))
--- Assets/_WorkSpace/Script/SceneChanger.cs
+++ Assets/_WorkSpace/Script/SceneChanger.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
-
     public void ChanegeScene(string StageName)
     {
         SceneManager.LoadScene(StageName);
