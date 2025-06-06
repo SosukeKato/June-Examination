0aeed06f97336ae4f35b262a88619bb274e55fd8
dokindokinpiano <dokindokinpiano@gmail.com>
2025-06-06 19:33:38 +0900

    [add]PlaySceneにGameManagerを追加

--- Assets/_WorkSpace/Script/PlayerCube.cs
+++ Assets/_WorkSpace/Script/PlayerCube.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerCube : MonoBehaviour
