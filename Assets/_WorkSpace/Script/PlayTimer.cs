61e023ce4cb957d2f60955352e493cef06d7a7ff
dokindokinpiano <dokindokinpiano@gmail.com>
2025-06-10 23:04:21 +0900

    [Update]PlayTimer.csの処理を追加

--- Assets/_WorkSpace/Script/PlayTimer.cs
+++ Assets/_WorkSpace/Script/PlayTimer.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayTimer : MonoBehaviour
 {
+    private float sec;
+    private float min;
+    private float hour;
+
+    [SerializeField]
+    Text dateTimeText;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,23 @@ public class PlayTimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //ƒ^ƒCƒ}[ƒXƒ^[ƒg
+        sec += Time.deltaTime;
+        //•b‚ª60•b‚æ‚èã‚¢‚Á‚½‚ç
+        if (sec > 60)
+        {
+            //•b‚ð‚O‚É‚·‚é
+            sec = 0;
+            //•ª‚ð‚Pƒvƒ‰ƒX‚·‚é
+            min++;
+        }
+        //•ª‚ª60‚ð‰z‚µ‚½‚ç
+        if (min >= 60)
+        {
+            //•ª‚ð‚O‚É‚·‚é
+            min = 0;
+            //Žž‚ð‚Pƒvƒ‰ƒX‚·‚é
+            hour++;
+        }
     }
 }
