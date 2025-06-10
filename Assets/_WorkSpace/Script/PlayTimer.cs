dbf80f7d42ba4c68b4e910cb22b0aa4e7f11bb97
dokindokinpiano <dokindokinpiano@gmail.com>
2025-06-10 22:46:28 +0900

    [add]PlaySceneにTimerTextを追加

--- Assets/_WorkSpace/Script/PlayTimer.cs
+++ Assets/_WorkSpace/Script/PlayTimer.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayTimer : MonoBehaviour
+public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //秒
+    private float sec;
+    //分
+    private float min;
+    //時
+    private float hour;
+    //タイマーテキスト
+    [SerializeField] Text dateTimeText;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //タイマースタート
+        sec += Time.deltaTime;
+        //秒が60秒より上いったら
+        if (sec > 60)
+        {
+            //秒を０にする
+            sec = 0;
+            //分を１プラスする
+            min++;
+        }
+        //分が60を越したら
+        if (min >= 60)
+        {
+            //分を０にする
+            min = 0;
+            //時を１プラスする
+            hour++;
+        }
+
+        //タイマーをテキストに反映
+        dateTimeText.text = hour.ToString("00") + ":" + min.ToString("00") + ":" + ((int)sec).ToString("00");
     }
 }
