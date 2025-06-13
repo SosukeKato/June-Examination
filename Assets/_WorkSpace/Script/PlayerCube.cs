f2b1635dd971e88febbad3e6f4f81736eded0881
SosukeKato <dokindokinpiano@gmail.com>
2025-06-13 17:20:48 +0900

    [fix]PlayerCube.csの不要な処理を削除

--- Assets/_WorkSpace/Script/PlayerCube.cs
+++ Assets/_WorkSpace/Script/PlayerCube.cs
@@ -1,7 +1,6 @@
 //using System.Collections;
 //using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerCube : MonoBehaviour
