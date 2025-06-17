ead96ce561ccc9fbeb528039ecab3f92ff0813e3
SosukeKato <dokindokinpiano@gmail.com>
2025-06-17 20:37:34 +0900

    [Update]1分ごとに_AttackIntervalと_AttackSpeedを上昇させる処理を追加

--- Assets/_WorkSpace/Script/EnemyShooting.cs
+++ Assets/_WorkSpace/Script/EnemyShooting.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyShooting : MonoBehaviour
@@ -8,12 +7,20 @@ public class EnemyShooting : MonoBehaviour
     [SerializeField]
     private GameObject _EnemyBulletPrefab;
     [SerializeField]
-    private float _AttackInterval = 0.5f;
-    [SerializeField]
     private List<Transform> _Muzzle;
+
+    private float _AttackInterval = 1f;
+    private int _AttackSpeed = 200;
 
     private float _timer;
     int r;
+
+    private void Start()
+    {
+        StartCoroutine("ShootingTimer");
+        StartCoroutine("ShootingTimer2");
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -23,8 +30,21 @@ public class EnemyShooting : MonoBehaviour
         {
             r = Random.Range(0, _Muzzle.Count);
             GameObject obj = Instantiate(_EnemyBulletPrefab, _Muzzle[r].position,Quaternion.identity);
-            obj.GetComponent<Rigidbody>().AddForce(_Muzzle[r].transform.forward * 500);
+            obj.GetComponent<Rigidbody>().AddForce(_Muzzle[r].transform.forward * _AttackSpeed);
             _timer = 0;
         }
+    }
+
+    IEnumerator ShootingTimer()
+    {
+        yield return new WaitForSeconds(60);
+        _AttackInterval = 0.7f;
+        _AttackSpeed = 300;
+    }
+    IEnumerator ShootingTimer2()
+    {
+        yield return new WaitForSeconds(120);
+        _AttackInterval = 0.5f;
+        _AttackSpeed = 500;
     }
 }
